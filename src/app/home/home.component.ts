b86e614d6bb3ab5bf1c268ea5365c4e3812be0b6
SimoneChiodo <simochi.005@gmail.com>
2025-11-11 17:49:01 +0100

    Add list to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  
+  elencoMacchine: string[] = ["Lamborghini", "Fiat", "BMW", "Subaru"]
 
 }
