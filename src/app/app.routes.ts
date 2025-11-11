8965ee1e5d58941522970a1c7fb8dabf5ff71d84
SimoneChiodo <simochi.005@gmail.com>
2025-11-11 17:17:25 +0100

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Pages
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+
+
+export const routes: Routes = [  
+    { path: "/home", component: HomeComponent },
+    { path: "/services", component: ServicesComponent },
+    { path: "/about-me", component: AboutMeComponent },
+    { path: "/contacts", component: ContactsComponent },
+];
